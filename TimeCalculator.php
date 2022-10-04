2b8a7c42da28e699c3c18b2720db6922871fb6d6
RAREGUN\\u0021 <fluxgore@gmail.com>
2022-10-04 22:31:03 +0300

    TimeCalculator input checks and hours limitation

--- TimeCalculator.php
+++ TimeCalculator.php
@@ -2,6 +2,8 @@
 
 class TimeCalculator
 {
+    static array $possibleCharacters = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':' ];
+
     static function timeToSeconds($time): int {
         list($hours, $minutes, $seconds) = explode(':', $time);
 
@@ -12,11 +14,23 @@ class TimeCalculator
         $hours = (int) ($seconds / 3600);
         $minutes = (int) (($seconds - ($hours * 3600)) / 60);
         $seconds = $seconds - $hours * 3600 - $minutes * 60;
+        $hours = $hours % 24;
 
         return $hours . ":" . str_pad($minutes, 2, '0', STR_PAD_LEFT) . ':' . str_pad($seconds, 2, '0', STR_PAD_LEFT);
     }
 
     static function sumTime($first, $second): string {
+        foreach (str_split($first) as $char)
+            if (!in_array($char, self::$possibleCharacters))
+                return 'Invalid input!';
+
+        foreach (str_split($second) as $char)
+            if (!in_array($char, self::$possibleCharacters))
+                return 'Invalid input!';
+
+        if (explode(':', $first) != 3 || explode(':', $second) != 3)
+            return 'Invalid input!';
+
         return self::secondsToTime(self::timeToSeconds($first) + self::timeToSeconds($second));
     }
 }
